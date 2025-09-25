f0ca4ef1419784d84864f5ad13c2c12a0526fdf2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-25 04:15:45 +0100

    Create pkg.go

---
+++ pkg.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/openai/openai-go/v2"
+	"github.com/openai/openai-go/v2/option"
+	"github.com/openai/openai-go/v2/shared"
+)
+
+func main() {
+	client := openai.NewClient(
+		option.WithAPIKey("My API Key"), // defaults to os.LookupEnv("OPENAI_API_KEY")
+	)
+	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage("Say this is a test"),
+		},
+		Model: openai.ChatModelGPT4o,
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	println(chatCompletion.Choices[0].Message.Content)
+}
